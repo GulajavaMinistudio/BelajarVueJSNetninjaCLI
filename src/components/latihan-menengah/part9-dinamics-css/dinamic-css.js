abcded8b00cc4ea4b0149c7d1cbff06da95c1a18
GulajavaMinistudio <gulajava.mini@gmail.com>
2018-02-26 12:15:25 +0700

    Belajar menggunakan dynamic css dengan vbind class

--- src/components/latihan-menengah/part9-dinamics-css/dinamic-css.js
+++ src/components/latihan-menengah/part9-dinamics-css/dinamic-css.js
@@ -2,14 +2,39 @@ export default {
   name: 'DynamicCSS',
   data() {
     return {
-
+      isTersedia: false,
+      isTerdekat: false,
     };
   },
   methods: {
-
+    setTersediaToggle() {
+      this.isTersedia = !this.isTersedia;
+    },
+    setTerdekatToggle() {
+      this.isTerdekat = !this.isTerdekat;
+    },
   },
   computed: {
-
+    /**
+     * @description Computed property untuk mengubah class CSS dengan binding
+     * yang bersifat dinamis, dan dapat diubah ubah dari kode
+     */
+    computedClass() {
+      return {
+        availableClass: this.isTersedia,
+        terdekatClass: this.isTerdekat,
+      };
+    },
+    classObjek1() {
+      return {
+        isClassObjek1Oke: true,
+      };
+    },
+    classObjek2() {
+      return {
+        isClassObjek2Oke: false,
+      };
+    },
   },
 };
 
