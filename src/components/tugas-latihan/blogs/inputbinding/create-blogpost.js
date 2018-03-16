0f577bd4c63477b584655b7baeeb277c57e848c2
GulajavaMinistudio <gulajava.mini@gmail.com>
2018-03-16 18:52:30 +0700

    Latihan membuat ajax request di vue

--- src/components/tugas-latihan/blogs/inputbinding/create-blogpost.js
+++ src/components/tugas-latihan/blogs/inputbinding/create-blogpost.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default {
   name: 'CreateBlogPost',
   data() {
@@ -6,11 +8,39 @@ export default {
         judul: '',
         konten: '',
         kategoris: [],
+        penulis: '',
       },
+      listPenulis: [
+        'Kucing Meow',
+        'Mumu The Cat',
+        'Munchkin Cat',
+      ],
+      isDataTerkirim: false,
+      isLoadingProg: false,
     };
   },
   methods: {
-
+    kirimBlogPesan() {
+      // kirim data via ajax dan axios
+      this.isLoadingProg = true;
+      axios.post('https://jsonplaceholder.typicode.com/posts', {
+        title: this.blog.judul,
+        bodyL: this.blog.konten,
+        userId: 1,
+      })
+        .then((resp) => {
+          console.log(resp.data);
+          this.isDataTerkirim = true;
+          this.isLoadingProg = false;
+        })
+        .catch(
+          (err) => {
+            console.log(err);
+            this.isDataTerkirim = false;
+            this.isLoadingProg = false;
+          },
+        );
+    },
   },
   computed: {
 
